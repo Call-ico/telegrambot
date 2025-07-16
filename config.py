93474107de059de8b64a71048f6c67f2417323c0
Call-ico <calicolilade@gmail.com>
2025-07-16 19:09:15 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,46 @@
-TELEGRAM_TOKEN = "token"  
+TELEGRAM_TOKEN = " token"  
+AUTOPOST_CHANNEL_ID = -1001261804062  
+
+AUTOPOST_HOUR = 21  
+AUTOPOST_MINUTE = 0
+
+MUSIC_POST_DAYS = 'mon,wed,fri'  
+MUSIC_POST_HOUR = 10  
+MUSIC_POST_MINUTE = 0
+
+CACHE_TTL = 60  
+MAX_TEXT_LENGTH = 32  
+RATE_LIMIT_SECONDS = 1  
+
+# Ключевые слова для поиска музыки
+MUSIC_KEYWORDS = [
+    "underground rap",
+    "drill",
+    "trap beat",
+    "russian rap",
+    "g-funk",
+    "old school hip hop",
+    "battle rap instrumental",
+    "dark trap",
+    "rage type beat",
+    "chill rnb",
+    "smooth rnb beat",
+    "rnb soul",
+    "90s rnb",
+    "trap soul",
+    "bedroom rnb",
+    "rnb vibes",
+    "emotional rnb",
+    "alternative rock",
+    "post rock",
+    "emotional rock",
+    "grunge",
+    "punk rock",
+    "indie rock",
+    "rock rap",
+    "nu metal",
+]
+
+
+
+MUSIC_MAX_DURATION_SEC = 300  
